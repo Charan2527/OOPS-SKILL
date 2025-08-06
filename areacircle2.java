3f834bdf5179a2781a6bbab3911d47387dc3ec36
Charan2527 <2420030397@klh.edu.in>
2025-08-06 10:50:06 +0530

    Add files via upload

---
+++ areacircle2.java
@@ -0,0 +1,11 @@
+package oops_skill;
+
+public class areacircle2 {
+	 public static void main(String[] args) {
+	        Circle c = new Circle();
+
+	        // Calling method with argument
+	        c.Area(2.5);  // Pass radius as parameter
+	    }
+
+}
