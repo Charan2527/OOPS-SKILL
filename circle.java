54352494fef777e9c80fd350546d16d7ab9e9ec2
Charan2527 <2420030397@klh.edu.in>
2025-08-06 10:47:44 +0530

    Add files via upload

--- circle.java
+++ circle.java
@@ -1,14 +1,12 @@
 package oops_skill;
-double radius = 5.0;
-void Area() {
-    double area = 3.14159 * radius * radius;
-    System.out.println("Radius: " + radius);
-    System.out.println("Area of the circle: " + area);
+class Circle {
+    double radius = 5.0;  
+
+    // Method without parameters
+    void Area() {
+        double area = 3.14159 * radius * radius;
+        System.out.println("Radius: " + radius);
+        System.out.println("Area of the circle: " + area);
+    }
 }
 
-public class circle {
-	 public static void main(String[] args) {
-	        Circle c = new Circle();
-	        c.Area(); 
-	 }
-}
